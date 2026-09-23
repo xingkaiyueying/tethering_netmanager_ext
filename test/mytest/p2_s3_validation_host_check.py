028b4f3f0d3125094f6c05785a667a1a5866bac6
xingkaiyueying <710429130@qq.com>
2026-09-23 14:17:38 +0800

    Fix Windows poll stub in S3 validation check

--- test/mytest/p2_s3_validation_host_check.py
+++ test/mytest/p2_s3_validation_host_check.py
@@ -27,6 +27,7 @@ constexpr int SOCK_CLOEXEC=0,MSG_NOSIGNAL=0,QEURY_TYPE_NETSYS=1;
 #ifndef POLLOUT
 constexpr short POLLOUT=0x0010;
 #endif
+struct pollfd {int fd; short events; short revents;};
 struct queryparam {int qp_netid=0,qp_type=0;};
 inline int activeFamily=0,failFamily=0,closed=0,bindError=0;
 inline size_t offset=0;
